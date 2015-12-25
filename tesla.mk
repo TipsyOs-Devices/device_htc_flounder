4dedbdfef81e38325b9fa7c22eeed9f8ae9cd5b4
slebit <tillnagotannat@gmail.com>
2015-12-25 11:57:54 +0100

    Use uber

--- tesla.mk
+++ tesla.mk
@@ -18,7 +18,7 @@ $(call inherit-product-if-exists, vendor/htc/flounder/device-vendor.mk)
 BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
 
 # Inline kernel building
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9-kernel/bin
 KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/htc/flounder
 TARGET_KERNEL_CONFIG := flounder_defconfig
